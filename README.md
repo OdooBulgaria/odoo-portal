ff9336c573acb624df68829ca042c7ab8017e3b1
Joaquin Gutierrez <joaquin@trey.es>
2015-02-25 20:32:11 +0100

    [FIX]: Menus, permisos y seguidores

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Crea un grupo llamado Portal Manager que tendra acceso a todas las opciones del
 portal.
 
 ## followers_portal
-Modulo para añadir como seguidores en los pedidos de ventas y las facturas a los usuarios
-que pertenezcan al grupo Portal Manager.
+Modulo para añadir como seguidores en los pedidos de ventas y las facturas, los 
+usuarios que pertenezcan al grupo Portal Manager.
 
--- followers_portal/models/sale.py
+++ followers_portal/models/sale.py
@@ -30,18 +30,47 @@ class SaleOrder(models.Model):
         result = super(SaleOrder, self).create(vals)
         resuers_obj = self.env['res.users']
         partner_obj = self.env['res.partner']
+        orders_obj = self.env['sale.order']
         followers_ids = []
         if 'partner_id' in vals:
-            partner_ids = partner_obj.browse(vals['partner_id'])
-            for partner in partner_ids.commercial_partner_id:
-                users = resuers_obj.search([('partner_id', '=', partner.id)])
-                for user in users:
-                    for group in user.groups_id:
-                        if group.is_portalmanager:
-                            followers_ids.append(user.id)
+            partner = partner_obj.browse(vals['partner_id'])
+            for contact in partner.child_ids:
+                user = resuers_obj.search([('partner_id', '=', contact.id)])
+                for group in user.groups_id:
+                    if group.is_portalmanager:
+                        followers_ids.append(contact.id)
             if followers_ids:
-                self.message_subscribe(self.id, followers_ids)
-                # subscribe the partner to the invoice
-                print "Final del Create"
+                order = orders_obj.browse(result.id)
+                for follower in order.message_follower_ids:
+                    followers_ids.append(follower.id)
+                    order.write({'message_follower_ids': [(6, 0, followers_ids)]})
+        return result
 
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.model
+    def create(self, vals):
+        result = super(AccountInvoice, self).create(vals)
+        resuers_obj = self.env['res.users']
+        partner_obj = self.env['res.partner']
+        invoice_obj = self.env['account.invoice']
+        followers_ids = []
+        if 'partner_id' in vals:
+            partner = partner_obj.browse(vals['partner_id'])
+            for contact in partner.child_ids:
+                user = resuers_obj.search([('partner_id', '=', contact.id)])
+                for group in user.groups_id:
+                    if group.is_portalmanager:
+                        followers_ids.append(contact.id)
+            if followers_ids:
+                print followers_ids
+                print result.id
+                print result
+                invoice = invoice_obj.browse(result.id)
+                for follower in invoice.message_follower_ids:
+                    followers_ids.append(follower.id)
+                    invoice.write({'message_follower_ids': [
+                        (6, 0, set(followers_ids))]})
         return result
